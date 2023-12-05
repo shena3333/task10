f072049d0f01186de01141ab8cf6519bfb40d2ff
shena3333 <shena3333@mail.ru>
2023-12-05 22:03:18 +0300

    redact portfolio

--- work10/src/pages/portfolio/portfolio.js
+++ work10/src/pages/portfolio/portfolio.js
@@ -5,18 +5,34 @@ function Portfolio() {
 
     return (
         <div className='portfolio-container'>
-            <p>Можно посмотреть примеры моих работ на Git:</p>
-            <ul>
-                <li>
-                    <a href="https://github.com/shena3333/project3Kondrikova">Первый пример</a>
-                </li>
-                <li>
-                    <a href="https://github.com/shena3333/task10">Второй пример</a>
-                </li>
-                <li>
-                    <a href="https://github.com/shena3333/calc">Третий пример</a>
-                </li>
-            </ul>
+            <div className='flex-container'>
+                <div className='content-container'>
+                    <p>Мои навыки</p>
+                    <ul>
+                        <li>создане макетов с помощью Figma;</li>
+                        <li>верстка страницы (HTML,CSS);</li>
+                        <li>разработка страницы с помощью JS;</li>
+                        <li>создание отзывчивого дизана страницы; </li>
+                        <li>создание сайта на основе CMS;</li>
+                        <li>создание сайта c помощью фреймворка React;</li>
+                        <li>контроль версий при помощи github.</li>
+                    </ul>
+                </div>
+                <div className='content-container'>
+                    <p>Примеры работ на Git:</p>
+                    <ul>
+                        <li>
+                            <a href="https://github.com/shena3333/project3Kondrikova">HTML,CSS</a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/shena3333/task10">React</a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/shena3333/calc">JS</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
 
         </div>
 
