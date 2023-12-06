4bef717f8c50300e0518e719f1441d54eb7f8baa
shena3333 <shena3333@mail.ru>
2023-12-06 23:34:42 +0300

    redact page photo

--- work10/src/pages/photo/photo.js
+++ work10/src/pages/photo/photo.js
@@ -1,14 +1,33 @@
 import './photo.css';
 import React from 'react';
+import foto1 from './1.bmp';
+import foto2 from './2.bmp';
+import foto3 from './3.bmp';
+import foto4 from './4.bmp';
 
 
 function Photo() {
 
+    // const mainFoto = document.querySelector('.main-foto');
+    // document.addEventListener('mouseover', function (e) {
+    //     if (e.target.closest('.foto')) {
+    //         mainFoto.src = e.target.src;
+    //     }
+    // });
 
     return (
         <>
-        <div className='container-photo'>
-            фотогалерея
+            <div className='container-photo'>
+                <div className='gallery-container'>
+                    <p className='gallery'>Галерея работ:</p>
+                </div>
+                {/* <div><img className='main-foto' src={foto1} alt="main-image" /></div> */}
+                <div className="foto-box">
+                    <img class="foto foto1" alt='icon' src={foto1} />
+                    <img class="foto foto2" alt='icon' src={foto2} />
+                    <img class="foto foto3" alt='icon' src={foto3} />
+                    <img class="foto foto4" alt='icon' src={foto4} />
+                </div>
             </div>
         </>
     )
